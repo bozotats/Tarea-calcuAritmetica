f50d7411426472b640d9f8690c21614d870d2bfe
installer <installer@gmail.com>
2024-09-21 08:48:34 +0900

    src files organizados

---
+++ src/calc.java
@@ -0,0 +1,38 @@
+import java.util.StringTokenizer;
+
+public class calc {
+  public static void main(String[] args) throws Exception {
+    StringTokenizer tkizer = new StringTokenizer(args[0], "+-*/()^", true);
+    boolean numflag = true;
+    String tempNumToken = "";
+    String tempToken = "";
+
+    while (tkizer.hasMoreTokens()){
+      tempToken = tkizer.nextToken();
+      switch (tempToken){
+        case "+":
+          break;
+        case "-":
+          break;
+        case "*":
+          break;
+        case "/":
+          break;
+        case "(":
+          break;
+        case ")":
+          break;
+        case "^":
+          break;
+        default:
+          if (tempToken.matches("[0-9]+")) {
+       
+          } else {
+       
+          }
+          break;
+      }
+    }
+  }
+}
+
